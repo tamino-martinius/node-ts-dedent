17023d1ec7926697bd74aefcc899b9e12d4c150f
Tamino Martinius <dev@zaku.eu>
2018-06-11 19:54:52 +0200

    Implement generic request to jira api

--- src/jira.ts
+++ src/jira.ts
@@ -34,6 +34,38 @@ export class Jira {
     }
   }
 
+  request(method: RequestMethod, rel: string, params: Dict<any> = {}, body: Dict<any> = {}): Promise<Response> {
+    return new Promise((resolve, reject) => {
+      const url = new URL(rel, this.apiBaseUrl);
+      const paramKeys: string[] = Object.keys(params).sort();
+      url.username = this.username;
+      url.password = this.password;
+      const auth = url.username && url.password ? `${url.username}:${url.password}` : undefined;
+      const options: https.RequestOptions = {
+        hostname: url.hostname,
+        protocol: url.protocol,
+        port: url.port,
+        path: url.pathname + Jira.paramsToQuery(params),
+        auth,
+        method,
+      };
+      // console.log(options, url.toJSON());
+
+      const req = https.request(options, res => {
+        res.on('data', buffer => resolve({
+          data: JSON.parse(buffer.toString()),
+          status: res.statusCode || 902,
+        }));
+        res.on('error', buffer => reject(JSON.parse(buffer.toString())));
+      });
+      const bodyKeys: string[] = Object.keys(body);
+      if ((method === RequestMethod.POST || method === RequestMethod.PUT) && bodyKeys.length > 0) {
+        req.write(JSON.stringify(body));
+      }
+      req.end();
+    });
+  }
+
 };
 
 export default Jira;
