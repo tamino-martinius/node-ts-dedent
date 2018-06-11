7a22699b98e64f9f12d4c575c45b0fbf909cba3c
Tamino Martinius <dev@zaku.eu>
2018-06-11 19:54:03 +0200

    Start with Jira class

--- src/jira.ts
+++ src/jira.ts
@@ -10,3 +10,21 @@ import {
 import { URL } from 'url';
 import https from 'https';
 
+export class Jira {
+  private url: URL;
+  private apiBaseUrl: URL;
+  private username: string;
+  private password: string;
+  private version: string;
+
+  constructor(config: JiraConfig) {
+    this.url = new URL(config.url);
+    this.username = config.username;
+    this.password = config.password;
+    this.version = config.version || '2';
+    this.apiBaseUrl = new URL(`rest/api/${this.version}/`, this.url);
+  }
+
+};
+
+export default Jira;
