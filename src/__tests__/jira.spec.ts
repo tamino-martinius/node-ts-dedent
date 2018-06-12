22c5a99f226bd97514a32aca607bd8a5f1eb9d2c
Tamino Martinius <dev@zaku.eu>
2018-06-12 16:55:33 +0200

    Add specs for editIssue

--- src/__tests__/jira.spec.ts
+++ src/__tests__/jira.spec.ts
@@ -5,6 +5,7 @@ import {
   Dict,
 } from '../';
 import https from 'https';
+import dedent from '../dedent';
 
 jest.mock('https');
 let responseStatus: number = 902;
@@ -57,6 +58,57 @@ describe('Jira', () => {
 
   const getJira = () => new Jira({ url, username, password });
 
+  describe('#editIssue', () => {
+    let updateHistory = false;
+    let fields = {};
+    let historyMetadata = undefined;
+    let issueList = undefined;
+    let properties = undefined;
+    let transition = undefined;
+    let update = undefined;
+    const subject = () => getJira().createIssue({
+      fields,
+      historyMetadata,
+      issueList,
+      properties,
+      transition,
+      update,
+    }, updateHistory);
+
+    it('makes call to jira API', async () => {
+      await subject();
+      expect(requestString).toEqual(dedent`
+        POST https://foo:bar@example.com/rest/api/2/issue?updateHistory=false
+        {"fields":{}}
+      `);
+    });
+
+    context('when issue is created', {
+      definitions() {
+        responseStatus = 201;
+        responseBody = { key: 'JIRA-1' };
+      },
+      tests() {
+        it('returns issue', async () => {
+          const issue = await subject();
+          expect(issue).toEqual(responseBody);
+        });
+      },
+    });
+
+    context('when issue is not created', {
+      definitions() {
+        responseStatus = 400;
+      },
+      tests() {
+        it('returns undefined', async () => {
+          const issue = await subject();
+          expect(issue).toBeUndefined();
+        });
+      },
+    });
+  })
+
   describe('#getIssue', () => {
     let issueKey = 'JIRA-1234';
     const subject = () => getJira().getIssue(issueKey);
@@ -93,5 +145,6 @@ describe('Jira', () => {
       },
     });
   })
+
 });
 
