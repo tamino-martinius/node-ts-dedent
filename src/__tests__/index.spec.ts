06f33ffe3982799b7d69c865d246846b566d3584
Tamino Martinius <dev@zaku.eu>
2019-07-26 08:16:28 +0200

    Applied linting

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -127,66 +127,78 @@ describe('dedent tag', () => {
     ).toEqual('1. line #1\n\t2. line #2\n\t\t3. line');
   });
 
-  it('should ignore the last line if it doesn\'t contain anything else than whitespace', () => {
+  it("should ignore the last line if it doesn't contain anything else than whitespace", () => {
     expect(
-      function () {
+      (() => {
         return dedent`
 					Line #1
 					Line #2
 					Line #3
 				`;
-      }(),
+      })(),
     ).toEqual('Line #1\nLine #2\nLine #3');
 
     expect(
-      function () {
+      (() => {
         return dedent`
 					Line #${1}
 					Line #${2}
 					Line #${3}
 				`;
-      }(),
+      })(),
     ).toEqual('Line #1\nLine #2\nLine #3');
 
     expect(
-      function () {
+      (() => {
         return dedent`
 					${1}. line #${1}
 					${2}. line #${2}
 					${3}. line
 				`;
-      }(),
+      })(),
     ).toEqual('1. line #1\n2. line #2\n3. line');
   });
 });
 
 describe('dedent() function', () => {
   it('should work with tabs', () => {
-    expect(dedent(`Line #1
+    expect(
+      dedent(`Line #1
 			Line #2
-			Line #3`)).toEqual('Line #1\nLine #2\nLine #3');
+			Line #3`),
+    ).toEqual('Line #1\nLine #2\nLine #3');
 
-    expect(dedent(`Line #${1}
+    expect(
+      dedent(`Line #${1}
 			Line #${2}
-			Line #${3}`)).toEqual('Line #1\nLine #2\nLine #3');
+			Line #${3}`),
+    ).toEqual('Line #1\nLine #2\nLine #3');
 
-    expect(dedent(`${1}. line #${1}
+    expect(
+      dedent(`${1}. line #${1}
 			${2}. line #${2}
-			${3}. line`)).toEqual('1. line #1\n2. line #2\n3. line');
+			${3}. line`),
+    ).toEqual('1. line #1\n2. line #2\n3. line');
   });
 
   it('should work with spaces', () => {
-    expect(dedent(`Line #1
+    expect(
+      dedent(`Line #1
             Line #2
-            Line #3`)).toEqual('Line #1\nLine #2\nLine #3');
+            Line #3`),
+    ).toEqual('Line #1\nLine #2\nLine #3');
 
-    expect(dedent(`Line #${1}
+    expect(
+      dedent(`Line #${1}
             Line #${2}
-            Line #${3}`)).toEqual('Line #1\nLine #2\nLine #3');
+            Line #${3}`),
+    ).toEqual('Line #1\nLine #2\nLine #3');
 
-    expect(dedent(`${1}. line #${1}
+    expect(
+      dedent(`${1}. line #${1}
             ${2}. line #${2}
-            ${3}. line`)).toEqual('1. line #1\n2. line #2\n3. line');
+            ${3}. line`),
+    ).toEqual('1. line #1\n2. line #2\n3. line');
   });
 
   it('should remove leading/trailing line break', () => {
@@ -273,66 +285,78 @@ describe('dedent() function', () => {
     ).toEqual('1. line #1\n\t2. line #2\n\t\t3. line');
   });
 
-  it('should ignore the last line if it doesn\'t contain anything else than whitespace', () => {
+  it("should ignore the last line if it doesn't contain anything else than whitespace", () => {
     expect(
-      function () {
+      (() => {
         return dedent(`
 					Line #1
 					Line #2
 					Line #3
 				`);
-      }(),
+      })(),
     ).toEqual('Line #1\nLine #2\nLine #3');
 
     expect(
-      function () {
+      (() => {
         return dedent(`
 					Line #${1}
 					Line #${2}
 					Line #${3}
 				`);
-      }(),
+      })(),
     ).toEqual('Line #1\nLine #2\nLine #3');
 
     expect(
-      function () {
+      (() => {
         return dedent(`
 					${1}. line #${1}
 					${2}. line #${2}
 					${3}. line
 				`);
-      }(),
+      })(),
     ).toEqual('1. line #1\n2. line #2\n3. line');
   });
 });
 
 describe('dedent() function with custom tag', () => {
   it('should work with tabs', () => {
-    expect(dedent(tag`Line #1
+    expect(
+      dedent(tag`Line #1
 			Line #2
-			Line #3`)).toEqual('Line #1\nLine #2\nLine #3');
+			Line #3`),
+    ).toEqual('Line #1\nLine #2\nLine #3');
 
-    expect(dedent(tag`Line #${1}
+    expect(
+      dedent(tag`Line #${1}
 			Line #${2}
-			Line #${3}`)).toEqual('Line #2\nLine #4\nLine #6');
+			Line #${3}`),
+    ).toEqual('Line #2\nLine #4\nLine #6');
 
-    expect(dedent(tag`${1}. line #${1}
+    expect(
+      dedent(tag`${1}. line #${1}
 			${2}. line #${2}
-			${3}. line`)).toEqual('2. line #2\n4. line #4\n6. line');
+			${3}. line`),
+    ).toEqual('2. line #2\n4. line #4\n6. line');
   });
 
   it('should work with spaces', () => {
-    expect(dedent(tag`Line #1
+    expect(
+      dedent(tag`Line #1
             Line #2
-            Line #3`)).toEqual('Line #1\nLine #2\nLine #3');
+            Line #3`),
+    ).toEqual('Line #1\nLine #2\nLine #3');
 
-    expect(dedent(tag`Line #${1}
+    expect(
+      dedent(tag`Line #${1}
             Line #${2}
-            Line #${3}`)).toEqual('Line #2\nLine #4\nLine #6');
+            Line #${3}`),
+    ).toEqual('Line #2\nLine #4\nLine #6');
 
-    expect(dedent(tag`${1}. line #${1}
+    expect(
+      dedent(tag`${1}. line #${1}
             ${2}. line #${2}
-            ${3}. line`)).toEqual('2. line #2\n4. line #4\n6. line');
+            ${3}. line`),
+    ).toEqual('2. line #2\n4. line #4\n6. line');
   });
 
   it('should remove leading/trailing line break', () => {
@@ -419,35 +443,35 @@ describe('dedent() function with custom tag', () => {
     ).toEqual('2. line #2\n\t4. line #4\n\t\t6. line');
   });
 
-  it('should ignore the last line if it doesn\'t contain anything else than whitespace', () => {
+  it("should ignore the last line if it doesn't contain anything else than whitespace", () => {
     expect(
-      function () {
+      (() => {
         return dedent(tag`
 					Line #1
 					Line #2
 					Line #3
 				`);
-      }(),
+      })(),
     ).toEqual('Line #1\nLine #2\nLine #3');
 
     expect(
-      function () {
+      (() => {
         return dedent(tag`
 					Line #${1}
 					Line #${2}
 					Line #${3}
 				`);
-      }(),
+      })(),
     ).toEqual('Line #2\nLine #4\nLine #6');
 
     expect(
-      function () {
+      (() => {
         return dedent(tag`
 					${1}. line #${1}
 					${2}. line #${2}
 					${3}. line
 				`);
-      }(),
+      })(),
     ).toEqual('2. line #2\n4. line #4\n6. line');
   });
 });
