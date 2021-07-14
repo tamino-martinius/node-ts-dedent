e616cea350c84a1235fb9a61fab6387eeb2846e5
CJ Winslow <cj.winslow@airbnb.com>
2021-07-14 13:47:37 -0700

    Add failing test for direct function calls

--- src/__tests__/issue-21.spec.ts
+++ src/__tests__/issue-21.spec.ts
@@ -41,4 +41,48 @@ describe('Issue 21', () => {
 
     expect(a).toEqual(expected);
   });
+
+  /**
+   * Could not find a way to handle this but is an edge case we'd like to solve eventually
+   */
+  it.skip('should handle function calls of nested dedents correctly', () => {
+    const fieldDocs = dedent(`
+      * a
+      * b
+      * c
+    `)
+
+    const a = dedent(`
+      /**
+       ${fieldIntro()}
+       *
+       ${fieldDocs}
+       *
+       ${fieldExample()}
+       */
+    `)
+
+    function fieldIntro() {
+      return dedent(`
+        * 0
+      `)
+    }
+    function fieldExample() {
+      return dedent(`
+        * d
+      `)
+    }
+
+    const expected = `/**
+ * 0
+ *
+ * a
+ * b
+ * c
+ *
+ * d
+ */`
+
+    expect(a).toEqual(expected);
+  });
 });
